75e891ea8ff67515002ac704c142f44677c1f9bd
digital <digital@MacBookPro.lan>
2025-04-05 17:29:44 +0200

    feat(front) Add login and register page

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -11,13 +11,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-
-final class HomeController extends AbstractController
+#[Route('/login')]
+final class LoginController extends AbstractController
 {
-    #[Route(name: '/', methods: ['GET'])]
+    #[Route(name: '', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('login/index.html.twig', [
 
         ]);
     }
--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -11,13 +11,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-
-final class HomeController extends AbstractController
+#[Route('/register')]
+final class RegisterController extends AbstractController
 {
-    #[Route(name: '/', methods: ['GET'])]
+    #[Route(name: '', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('register/index.html.twig', [
 
         ]);
     }
